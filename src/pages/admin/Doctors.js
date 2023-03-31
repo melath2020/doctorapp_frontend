94581f1762e41e41049bd072c5c06c24c5dcc461
Aswin s <aswinsudhakaran98@gmail.com>
2023-03-31 23:33:54 +0530

    updte

--- src/pages/admin/Doctors.js
+++ src/pages/admin/Doctors.js
@@ -1,10 +1,94 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import Layout from '../../components/Layout'
+import { message, Table } from 'antd'
+
 
 const Doctors = () => {
+  const [doctor,setDoctor]=useState([])
+  // getUsers
+  const getDoctors=async()=>{
+    try{
+      const res= await axios.get('http://localhost:8000/api/v1/admin/getAllDoctors',{
+        headers:{
+          Authorization:`Bearer ${localStorage.getItem('token')}` 
+        }
+      })
+      if(res.data.success){
+        setDoctor(res.data.data)
+      }
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  const handleAccountStatus=async(record,status)=>{
+    try{
+      const res=await axios.post('http://localhost:8000/api/v1/admin/changeAccountStatus',{doctorId:record._id,userId:record.userId,status:status},{
+        headers:{
+          Authorization:`Bearer ${localStorage.getItem('token')}`
+        }
+      })
+      if(res.data.success){
+      
+        message.success(res.data.message)
+        window.location.reload()
+      }
+
+    }catch(error){
+      message.error('Something Went Wrong')
+    }
+
+  }
+  useEffect(()=>{
+    getDoctors()
+  },[])
+
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render:(text,record)=>(
+        <span>{record.firstname} {record.lastname}</span>
+      )
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'phone',
+      key: 'phone',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      key: 'actions',
+      render:(text,record)=>(
+        <div className='d-flex'>
+          {record.status === 'pending' ? <button className='btn btn-success' onClick={()=>handleAccountStatus(record,'approved')}>Approve</button>:
+          <button className='btn btn-danger'>Reject</button> }
+
+        </div>
+      )
+    },
+  ];
   return (
-    <Layout><h1>All Doctors</h1></Layout>
+    <Layout><h1>All Doctors</h1>
+    <Table dataSource={doctor} columns={columns} />;
+    </Layout>
   )
 }
 
--- src/pages/admin/Users.js
+++ src/pages/admin/Users.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import { Table } from 'antd'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import Layout from '../../components/Layout'
 
+
 const Users = () => {
+  const [user,setUser]=useState([])
+  // getUsers
+  const getUsers=async()=>{
+    try{
+      const res= await axios.get('http://localhost:8000/api/v1/admin/getAllUsers',{
+        headers:{
+          Authorization:`Bearer ${localStorage.getItem('token')}` 
+        }
+      })
+      if(res.data.success){
+        setUser(res.data.data)
+      }
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    getUsers()
+  },[])
+
+  
+  
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Doctor',
+      dataIndex: 'isDoctor',
+      key: 'doctor',
+      render:(text,record)=>(
+        <span>{record.isDoctor? 'Yes': 'No'}</span>
+      )
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      key: 'actions',
+      render:(text,record)=>(
+        <div className='d-flex'>
+          <button className='btn btn-danger'>Block</button>
+        </div>
+      )
+    },
+  ];
+  
   return (
-    <Layout><h1>Users List</h1></Layout>
+    <Layout><h1 className='text-center m-2'>Users List</h1>
+    <Table dataSource={user} columns={columns} />;
+
+    </Layout>
   )
 }
 
